0ee00e1cc80ef25c5733a00722ead14f152837ec
MaceChettiyadan <mace.chettiyadan@gmail.com>
2022-09-06 12:38:15 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import plotly
+import requests
+import tkinter
+from bs4 import BeautifulSoup
+
+#tkinter set up two inputs and a button
+#plotly view below
+#maybe a mode where you can use some sort of online database
