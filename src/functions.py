4e6109e167b5e54d155af01f59d4f40be0d89d9f
MaceChettiyadan <mace.chettiyadan@gmail.com>
2022-09-12 18:10:40 +0800

    hello mr wilson

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,43 @@
 import webbrowser
 import wikipedia
 import webbrowser
+from bs4 import BeautifulSoup as bs
 
 def get_page(name):
-  return wikipedia.page(name, auto_suggest=False, redirect=True, preload=False)
+  return wikipedia.page(name)
   
 def open_page(name):
   url = wikipedia.page(name).url
   webbrowser.open(url)
+
+def extract_headers(html):
+  # return headers from html string
+  soup = bs(html, 'html.parser')
+  try:
+    dic = {}
+    toc = soup.find(id="toc")
+    lis = toc.find_all('li')
+    for li in lis:
+      #get child spans
+      nums = li.find_all(class_ = "tocnumber")
+      num = nums[0].text
+      values = li.find_all(class_ = "toctext")
+      value = values[0].text
+      dic[num] = value
+      
+    treeview_data = []
+    for key in dic:
+      val = dic[key]
+      parent = key.split(".")[0] if '.' in key else ''
+      id = key
+      treeview_data.append((parent, id, val, (key, val)))
+    return treeview_data
+  except:
+    return [("", 0, "No headers found!", ("", "This article has no Headers"))]
+    
+    
+      
+    
+  
+  
+  
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 
+import math
 import tkinter as tk
 from tkinter import ttk
 import functions as f
 import sv_ttk
-import plotly
+import matplotlib.pyplot as plt
+import networkx as nx
+import tkinter as tk
+import matplotlib
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg,
+    NavigationToolbar2Tk
+)
+matplotlib.use('TkAgg')
 
 class App(ttk.Frame):
     def __init__(self, parent):
@@ -13,13 +22,35 @@ class App(ttk.Frame):
         for index in (0, 1, 2, 3, 4, 5, 6, 7):
             self.columnconfigure(index=index, weight=1)
             self.rowconfigure(index=index, weight=1)
-
+            
+        self.graphs = []
         # Create widgets
         self.setup_gui()
         
     def submit_wiki_name(self):
         name = self.lookup_input.get()
         page = f.get_page(name)
+        treeview_data = f.extract_headers(page.html())
+        for item in treeview_data:
+            parent, iid, text, values = item
+            self.treeview.insert(
+                parent=parent, index="end", iid=iid, text=text, values=values
+            )
+
+            if not parent or iid in {8, 21}:
+                self.treeview.item(iid, open=True)  # Open parents
+
+        # Select and scroll
+        i = len(treeview_data)
+        while i > 0:
+            try:
+                self.treeview.selection_set(str(len(treeview_data) - 1))
+                break
+            except tk.TclError:
+                i -= 1
+                continue
+        self.treeview.see("7")
+        
         title = page.title
         #set title of wiki info
         self.wiki_info_title.config(text=title)
@@ -32,6 +63,65 @@ class App(ttk.Frame):
             tss = page.summary.split(".")[0] + "."
         self.wiki_info_summary.config(text=tss)
         self.wiki_info_popularity.config(text="Popularity: " + str(len(page.links)))
+        
+        self.setup_graph_view(page.title, page.links)
+        
+    def setup_graph_view(self, centre, links):
+        #remove old graph
+        for graph in self.graphs:
+            graph.destroy()
+            
+        
+        #create graph G with centre node and links, each node has a position
+        G = nx.Graph()
+        G.add_node(centre, pos=(0, 0))
+        #generate array of tuples of positions in circle
+        poss = []
+        for x in range(len(links)):
+            poss.append((0.5 * math.cos(2 * math.pi * x / len(links)), 0.5 * math.sin(2 * math.pi * x / len(links))))
+        
+        for link in links:
+            G.add_node(link, pos=poss[links.index(link)])
+            G.add_edge(centre, link)
+        
+        
+        
+        # position is stored as node attribute data for random_geometric_graph
+        pos = nx.get_node_attributes(G, "pos")
+        
+        dmin = 1
+        ncenter = 0
+        for n in pos:
+            x, y = pos[n]
+            d = (x - 0.5) ** 2 + (y - 0.5) ** 2
+            if d < dmin:
+                ncenter = n
+                dmin = d
+        
+        # color by path length from node near center
+        p = dict(nx.single_source_shortest_path_length(G, ncenter))
+        
+        fig = plt.figure(figsize=(4, 6))
+        nx.draw_networkx_edges(G, pos, nodelist=[ncenter], alpha=0.4)
+        nx.draw_networkx_nodes(
+            G,
+            pos,
+            nodelist=list(p.keys()),
+            node_size=80,
+            node_color=list(p.values()),
+            cmap=plt.cm.Reds_r,
+        )
+
+        plt.xlim(-0.05, 1.05)
+        plt.ylim(-0.05, 1.05)
+        plt.axis("off")
+        figure_canvas = FigureCanvasTkAgg(fig, self.graph_view_frame)
+        nav = NavigationToolbar2Tk(figure_canvas, self.graph_view_frame)
+        figure_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+        self.graphs.append(figure_canvas.get_tk_widget())
+        #append navigation toolbar to graph view frame
+        self.graphs.append(nav)
+        
 
     def setup_gui(self):
         #title frame
@@ -70,6 +160,14 @@ class App(ttk.Frame):
         #lookup how to label
         self.lookup_desc_label = ttk.Label(self.lookup_frame, text="Hint: Try not to use spaces. For example 'AmongUs' instead of 'Among Us'.", font=("-size", 10))
         self.lookup_desc_label.grid(row=1, column=0, columnspan=2, sticky="nsew")
+        #seperator row 2
+        self.warning_seperator = ttk.Separator(self.lookup_frame, orient="horizontal")
+        self.warning_seperator.grid(row=2, column=0, columnspan=2, sticky="ew", pady=(10, 10))
+        self.lookup_warning_label = ttk.Label(self.lookup_frame, text="Also, do not click anywhere when the program is looking up a wiki.", font=("-size", 10))
+        self.lookup_warning_label.grid(row=3, column=0, columnspan=2, sticky="nsew")
+        self.label_p2 = ttk.Label(self.lookup_frame, text="Lookup takes time (downloading data) and python is not multithreaded so it crashes.", font=("-size", 10))
+        self.label_p2.grid(row=4, column=0, columnspan=2, sticky="nsew")
+        
         
         #wiki info frame
         self.wiki_info_frame = ttk.LabelFrame(self, text="Wiki Info", padding=(10, 10))
@@ -95,13 +193,30 @@ class App(ttk.Frame):
             row=0, column=3, padx=(20, 10), pady=(20, 10), sticky="nsew", rowspan=4, columnspan=6
         )
         
+        self.scrollbar = ttk.Scrollbar(self.wiki_headers_frame)
+        self.scrollbar.pack(side="right", fill="y")
+        
+        self.treeview = ttk.Treeview(
+            self.wiki_headers_frame,
+            columns=("1", "2"),
+            height=10,
+            selectmode="browse",
+            show=("tree",),
+            yscrollcommand=self.scrollbar.set,
+        )
+        self.treeview.pack(expand=True, fill="both")
+        self.scrollbar.config(command=self.treeview.yview)
+
+        # Treeview columns
+        self.treeview.column("#0", anchor="w", width=120)
+        self.treeview.column(1, anchor="w", width=120)
+        self.treeview.column(2, anchor="w", width=120)
+        
         #graph view frame
         self.graph_view_frame = ttk.LabelFrame(self, text="Graph View", padding=(20, 10))
         self.graph_view_frame.grid(
             row=4, column=3, padx=(20, 10), pady=(20, 10), sticky="nsew", rowspan=4, columnspan=6
         )
-    
-
 
 def main():
     root = tk.Tk()
@@ -122,9 +237,16 @@ def main():
     root.minsize(width, height)
     root.geometry(f"+{x}+{y}")
     # set root fullscreen
-    root.attributes("-fullscreen", True)
+    #root.attributes("-fullscreen", True)
     root.mainloop()
 
 
 if __name__ == "__main__":
     main()
+    
+    
+#todo
+#multithreading
+#dark theme support with graph view
+#images for articles
+#full stop bug
